78be1b4add57e2799ed0a8c4beb834c5f8389473
therapeuti <lorraine_dubled@gmail.com>
2025-09-16 11:04:18 +0900

    "Remove django-debug-toolbar and fix   static files config

--- restaurant_system/settings.py
+++ restaurant_system/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = []
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
